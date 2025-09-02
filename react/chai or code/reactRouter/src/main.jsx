bf787adda245758a8ea8236b596ec4b58062c2d4
Lord-Rahul <rahul717321+github@gmail.com>
2025-09-02 10:52:36 +0530

    feat: Implement basic layout and routing for the React application
    - Created Layout component to wrap Header, Footer, and Outlet for nested routes.
    - Added About component with structured content and responsive design.
    - Developed Contact component with a form for user inquiries and contact details.
    - Implemented Footer component with navigation links and social media icons.
    - Created Header component with navigation links for Home, About, and Contact pages.
    - Developed Home component with promotional content and download link.
    - Set up index.js to export components for easier imports.
    - Added Tailwind CSS for styling and layout management.
    - Configured main.jsx for routing using React Router.
    - Set up Vite configuration for React and Tailwind CSS integration.

---
+++ react/chai or code/reactRouter/src/main.jsx
@@ -0,0 +1,26 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Layout from "./Layout.jsx";
+import { Home,About,Contact } from "./components/index.js";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+    </Route>
+  )
+);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
