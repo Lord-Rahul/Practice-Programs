c07cc73986eebeab1e5adf49c9e5fced8acbdef9
Lord-Rahul <rahul717321+github@gmail.com>
2025-09-02 22:16:02 +0530

    feat: initialize redux toolkit todo application with Vite and React
    - Added ESLint configuration for code quality and consistency.
    - Created index.html as the entry point for the application.
    - Set up package.json with necessary dependencies and scripts for development.
    - Developed main App component to include AddTodo and Todos components.
    - Configured Redux store to manage todo state.
    - Implemented AddTodo component for adding new todos.
    - Created Todos component to display and delete existing todos.
    - Developed todoSlice for managing todo actions and state.
    - Added Tailwind CSS for styling the application.
    - Set up main entry point (main.jsx) for rendering the application.
    - Configured Vite for building and serving the application.

---
+++ react/chai or code/reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -0,0 +1,32 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [
+    {
+      id: 1,
+      text: "hello world",
+    },
+  ],
+};
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload.text,
+      };
+      state.todos.push(todo);
+    },
+    deleteTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload.id);
+    },
+    updatedTodo: (state, action) => {},
+  },
+});
+
+export const { addTodo, deleteTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
