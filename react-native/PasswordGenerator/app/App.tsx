7813c6b8052fca5a72ecbeca8bae5741b1f7c863
lord-rahul <rahul.23ucs087.btcs@baddiuniv.ac.in>
2025-11-20 07:38:56 +0530

    feat: implement password generation logic in createPassword function

--- react-native/PasswordGenerator/app/App.tsx
+++ react-native/PasswordGenerator/app/App.tsx
@@ -20,7 +20,14 @@ export default function App() {
 
   const generatedPassword = (passwordLength: number) => {};
 
-  const createPassword = (characters: string, passwordLength: number) => {};
+  const createPassword = (characters: string, passwordLength: number) => {
+    let result = "";
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characters.length);
+      result += characters.charAt(characterIndex);
+    }
+    return result;
+  };
 
   const resetPassword = () => {};
   return (
