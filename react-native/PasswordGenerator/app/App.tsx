397967f52bbf3f41b735d84bd988f53f901dc192
lord-rahul <rahul.23ucs087.btcs@baddiuniv.ac.in>
2025-11-20 07:23:56 +0530

    feat: add yup validation schema for password length and update dependencies

--- react-native/PasswordGenerator/app/App.tsx
+++ react-native/PasswordGenerator/app/App.tsx
@@ -1,10 +1,17 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import * as yup from "yup";
+
+const passwordSchema = yup.object().shape({
+    passwordLength : yup.number()
+    .min(4,"password should be atleast 8 digit")
+    .required("this is a required feild")
+})
 
 export default function App() {
   return (
     <View>
       <Text>App</Text>
     </View>
-  )
+  );
 }
