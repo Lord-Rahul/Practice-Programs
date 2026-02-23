a2c3135316a0b82cc02c35cccf8087a5ef29b949
lord-rahul <rahul.23ucs087.btcs@baddiuniv.ac.in>
2026-02-23 11:45:28 +0530

    Refactor Home component: add styles and update view structure for SVG elements

--- react-native/Graphical-Primitives/app/Pages/Home.tsx
+++ react-native/Graphical-Primitives/app/Pages/Home.tsx
@@ -1,10 +1,18 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { StyleSheet } from "react-native";
 import Svg, { Line, Rect, Circle } from "react-native-svg";
 
 const Home = () => {
+
+  const styles = StyleSheet.create({
+    bg: {
+      backgroundColor: "#90EE90",
+    },
+  });
+
   return (
-    <View>
+    <View style={styles.bg}>
       <Svg width={"500"} height={"700"}>
         <Rect
           x={130}
@@ -57,7 +65,7 @@ const Home = () => {
           stroke={"black"}
         />
 
-            <Rect
+        <Rect
           x={79}
           y={160}
           width={"200"}
@@ -67,7 +75,7 @@ const Home = () => {
           fill={"#2986CC"}
         />
 
-         <Line
+        <Line
           x1={79}
           x2={30}
           y1={160}
@@ -75,7 +83,7 @@ const Home = () => {
           strokeWidth={"4"}
           stroke={"black"}
         />
-            <Line
+        <Line
           x1={279}
           x2={340}
           y1={160}
@@ -84,7 +92,7 @@ const Home = () => {
           stroke={"black"}
         />
 
-             <Line
+        <Line
           x1={230}
           x2={230}
           y1={340}
@@ -93,7 +101,7 @@ const Home = () => {
           stroke={"black"}
         />
 
-              <Line
+        <Line
           x1={130}
           x2={130}
           y1={340}
